256e29800ea1d46722f59bc85d051182d87af068
Bruh3509 <iutrushkin@bk.ru>
2024-03-07 21:01:43 +0300

    early version

---
+++ migrations/sql/create_chat_table.sql
@@ -0,0 +1,10 @@
+create table if not exists chat
+(
+    chat_id bigserial PRIMARY KEY,
+    user_name varchar(255) NOT NULL
+);
+
+
+
+
+
---
+++ migrations/sql/create_link_table.sql
@@ -0,0 +1,5 @@
+create table if not exists link
+(
+    link_id bigserial PRIMARY KEY,
+    link_name varchar(255) NOT NULL
+);
