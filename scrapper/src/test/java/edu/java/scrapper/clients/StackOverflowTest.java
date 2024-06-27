8dde87317e4d875845ad8f0570ca3f31baa8c727
Bruh3509 <iutrushkin@bk.ru>
2024-06-27 18:00:17 +0300

    checkstyle fixes

--- scrapper/src/test/java/edu/java/scrapper/clients/StackOverflowTest.java
+++ scrapper/src/test/java/edu/java/scrapper/clients/StackOverflowTest.java
@@ -1,20 +1,16 @@
 package edu.java.scrapper.clients;
 
 import edu.java.scrapper.configuration.ClientConfig;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = ClientConfig.class)
-//@SpringBootTest
 public class StackOverflowTest {
     @Autowired
     @Qualifier("stackoverflow")
